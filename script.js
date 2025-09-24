ffc7bfe0d844d2754ddfc3bd1bceb2411a781f84
Sebastian Brickel <sebastian.brickel@yahoo.com>
2025-09-24 11:57:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const track = document.querySelector('.carousel-track');
 const prevBtn = document.querySelector('.carousel-btn.prev');
 const nextBtn = document.querySelector('.carousel-btn.next');
+const dots = document.querySelectorAll('.carousel-indicators .dot');
 let index = 0;
 
 function updateCarousel() {
   track.style.transform = `translateX(-${index * 100}%)`;
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
 }
 
 nextBtn.addEventListener('click', () => {
@@ -15,6 +19,13 @@ nextBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', () => {
   index = (index - 1 + track.children.length) % track.children.length;
   updateCarousel();
+});
+
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    index = i;
+    updateCarousel();
+  });
 });
 
 // Auto-play every 5 seconds
