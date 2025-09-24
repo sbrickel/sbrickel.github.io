10f3eed39520baccf07cc0e2a775fdce30b58473
Sebastian Brickel <sebastian.brickel@yahoo.com>
2025-09-24 11:28:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ const prevBtn = document.querySelector('.carousel-btn.prev');
 const nextBtn = document.querySelector('.carousel-btn.next');
 let index = 0;
 
+function updateCarousel() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
 nextBtn.addEventListener('click', () => {
   index = (index + 1) % track.children.length;
-  track.style.transform = `translateX(-${index * 100}%)`;
+  updateCarousel();
 });
 
 prevBtn.addEventListener('click', () => {
   index = (index - 1 + track.children.length) % track.children.length;
-  track.style.transform = `translateX(-${index * 100}%)`;
+  updateCarousel();
 });
+
+// Auto-play every 5 seconds
+setInterval(() => {
+  index = (index + 1) % track.children.length;
+  updateCarousel();
+}, 5000);
